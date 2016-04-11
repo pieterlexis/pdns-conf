54a04644947bb9ae7160930394f4b9cb218f29ea
Pieter Lexis <pieter@plexis.eu>
2016-04-11 23:54:21 +0200

    Added a fuckton of config

---
+++ 3/stuff.sql
@@ -0,0 +1,2 @@
+INSERT INTO records (domain_id, name, content, type,ttl,prio)
+VALUES (2,'test.plexis.eu','1 54343 5 YWIwgZ8wDQYJKoZIhvcNAQEBBQADgY0AMIGJAoGBALPdN8L3zyMyyl3t','CERT',86400,NULL);
---
+++ recursor-rpz/rpz.lua
@@ -0,0 +1,2 @@
+-- rpzMaster("199.168.90.52", "rpz.spamhaus.org", {refresh=30})
+rpzMaster("67.207.213.63", "TestRPZ.PowerDNS.rpz.threatstop.local", {tsigalgo="hmac-md5", tsigname="threatstop-threatstop", tsigsecret="VsumFOvJ9fbrKFjBCGd+BLAmTMbi/HIG3tAudgoepYw7KMQmP24Fh09uQEkJEB0rM1ELDa8CUUKNULIyjrapzw==", refresh=30})
